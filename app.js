f903c6c056626da1a6e2e4e85ed6ff01ac1df32a
Vishesh <visheshsingh16@gmail.com>
2017-10-18 19:44:25 -0400

    Events

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-// NODES
+// EVENTS
+ const delbuttons = document.querySelectorAll('#book-list .delete')
 
-/* const banner = document.querySelector('#page-banner')
-console.log('Type of the banner node is:', banner.nodeType)
-console.log('Name of the banner node is:', banner.nodeName)
-console.log('Does banner node has children:', banner.hasChildNodes())
+ delbuttons.forEach(function(btn) {
+     btn.addEventListener('click', function(e) {
+         const li = e.target.parentElement;
+         li.parentNode.removeChild(li);
+     })
+ })
 
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner) */
-
-// Traversing the DOM - Parent and child nodes
-
-const booklist = document.querySelector('#book-list')
-
-console.log('The parent node is:', booklist.parentNode)
-console.log('The parent node is:', booklist.parentElement.parentElement)
-
-console.log('The child node is:', booklist.children)
-
-// Traversing the DOM - Sibling elements
-console.log('The next sibling is:', booklist.nextSibling)
-console.log('The next sibling is:', booklist.nextElementSibling)
-
-console.log('The next sibling is:', booklist.previousSibling)
-console.log('The next sibling is:', booklist.previousElementSibling)
-
-booklist.previousElementSibling.querySelector('p').innerHTML += '<br/>Get your favorite books here'

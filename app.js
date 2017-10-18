21467268e48bafaac1863959fbc60055fcbe9898
Vishesh <visheshsingh16@gmail.com>
2017-10-18 19:53:05 -0400

    Event Bubbling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // EVENTS
- const delbuttons = document.querySelectorAll('#book-list .delete')
+/*  const delbuttons = document.querySelectorAll('#book-list .delete')
 
  delbuttons.forEach(function(btn) {
      btn.addEventListener('click', function(e) {
@@ -7,4 +7,13 @@
          li.parentNode.removeChild(li);
      })
  })
+ */
 
+ const list = document.querySelector('#book-list ul');
+
+ list.addEventListener('click', function(e) {
+     if(e.target.className == 'delete') {
+         const li = e.target.parentElement;
+         list.removeChild(li);
+     }
+ })

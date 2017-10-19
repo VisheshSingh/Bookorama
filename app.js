0bff4080e075963586b5b471d178d143842803ae
Vishesh <visheshsingh16@gmail.com>
2017-10-18 21:51:32 -0400

    Custom search filter

--- app.js
+++ app.js
@@ -58,3 +58,20 @@ hideBox.addEventListener('change', function() {
         list.style.display = 'initial';
     }
 })
+
+//Custom Search Filter
+const search = document.forms['search-books'];
+
+search.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function(book) {
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) != -1) {
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    })
+})
+

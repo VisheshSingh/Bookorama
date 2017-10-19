eeea8f80dbe48cf5f585a53a81c9a0c875b5a721
Vishesh <visheshsingh16@gmail.com>
2017-10-18 21:11:22 -0400

    Checkboxes and change events

--- app.js
+++ app.js
@@ -46,3 +46,15 @@
 
      list.appendChild(li);
  })
+
+ //hide books
+
+const hideBox = document.querySelector('#hide');
+
+hideBox.addEventListener('change', function() {
+    if(hideBox.checked){
+        list.style.display = 'none';
+    } else {
+        list.style.display = 'initial';
+    }
+})
